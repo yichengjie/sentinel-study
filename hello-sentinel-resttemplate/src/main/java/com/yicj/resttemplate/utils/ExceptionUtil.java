b9c9ae38ab73a94b61862653174dcc1d6f935d2e
易成杰 <626659321@qq.com>
2023-09-16 14:10:37 +0800

    resttemplate sentinel test

--- hello-sentinel-resttemplate/src/main/java/com/yicj/resttemplate/utils/ExceptionUtil.java
+++ hello-sentinel-resttemplate/src/main/java/com/yicj/resttemplate/utils/ExceptionUtil.java
@@ -13,13 +13,13 @@ public class ExceptionUtil {
 
     public static ClientHttpResponse handleBlock(
             HttpRequest request, byte[] body, ClientHttpRequestExecution execution, BlockException be){
-        RestResponse<Void> commonResult =  RestResponse.error("500","降级处理函数。。。。。");
+        RestResponse<Void> commonResult =  RestResponse.error("500","降级处理函数 block 。。。。。");
         return new SentinelClientHttpResponse(JSON.toJSONString(commonResult));
     }
 
     public static ClientHttpResponse fallBack(
             HttpRequest request, byte[] body, ClientHttpRequestExecution execution, BlockException be){
-        RestResponse<Void> commonResult = RestResponse.error("500","异常处理函数。。。。。");
+        RestResponse<Void> commonResult = RestResponse.error("500","异常处理函数 fallback 。。。。。");
         return new SentinelClientHttpResponse(JSON.toJSONString(commonResult));
     }
 }
