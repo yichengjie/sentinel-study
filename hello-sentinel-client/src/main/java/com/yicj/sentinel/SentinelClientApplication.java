d54779cb965661550e304d314433aaa8ee8d07e3
易成杰 <626659321@qq.com>
2023-09-16 15:07:27 +0800

    gateway start

--- hello-sentinel-client/src/main/java/com/yicj/sentinel/SentinelClientApplication.java
+++ hello-sentinel-client/src/main/java/com/yicj/sentinel/SentinelClientApplication.java
@@ -3,6 +3,9 @@ package com.yicj.sentinel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author: yicj
@@ -16,4 +19,13 @@ public class SentinelClientApplication {
 
         SpringApplication.run(SentinelClientApplication.class, args) ;
     }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
 }
+
