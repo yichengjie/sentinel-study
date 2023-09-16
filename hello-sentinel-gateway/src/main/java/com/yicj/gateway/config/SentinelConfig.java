3939aa546fb1c5c9fe9bd5b1e6e2d1a52d830569
易成杰 <626659321@qq.com>
2023-09-16 15:48:18 +0800

    gateway start

---
+++ hello-sentinel-gateway/src/main/java/com/yicj/gateway/config/SentinelConfig.java
@@ -0,0 +1,142 @@
+package com.yicj.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayParamFlowItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author yicj
+ * @date 2023年09月13日 12:58
+ */
+@Configuration
+public class SentinelConfig {
+
+
+//    public final List<ViewResolver> viewResolvers ;
+//    private final ServerCodecConfigurer serverCodecConfigurer;
+//
+//
+//    public SentinelConfig(
+//            ObjectProvider<List<ViewResolver>> viewResolversProvider,
+//            ServerCodecConfigurer serverCodecConfigurer) {
+//        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+//        this.serverCodecConfigurer = serverCodecConfigurer;
+//    }
+//
+//
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+//        // Register the block exception handler for Spring Cloud Gateway.
+//        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+//    }
+//
+//    @Bean
+//    @Order(-1)
+//    public GlobalFilter sentinelGatewayFilter() {
+//        return new SentinelGatewayFilter();
+//    }
+
+    @PostConstruct
+    public void init(){
+        this.initCustomizeRule();
+        ////////////////
+        //this.initCustomizedApis();
+    }
+
+    private void initCustomizeRule(){
+        Set<GatewayFlowRule> list = new HashSet<>() ;
+        GatewayFlowRule rule = new GatewayFlowRule("hello-nacos-client") ;
+        rule.setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_ROUTE_ID) ;
+        // qps: 1
+        rule.setGrade(1) ;
+        rule.setCount(1) ;
+        rule.setIntervalSec(1) ;
+        rule.setControlBehavior(1) ;
+        list.add(rule) ;
+
+        GatewayRuleManager.loadRules(list) ;
+    }
+
+    private void initCustomizedApis() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+        ApiDefinition api1 = new ApiDefinition("some_customized_api")
+                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
+                    add(new ApiPathPredicateItem().setPattern("/product/baz"));
+                    add(new ApiPathPredicateItem().setPattern("/product/foo/**")
+                            .setMatchStrategy(SentinelGatewayConstants.PARAM_MATCH_STRATEGY_PREFIX));
+                }});
+        ApiDefinition api2 = new ApiDefinition("another_customized_api")
+                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
+                    add(new ApiPathPredicateItem().setPattern("/ahas"));
+                }});
+        definitions.add(api1);
+        definitions.add(api2);
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+    }
+
+    private void initGatewayRules() {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+        rules.add(new GatewayFlowRule("aliyun_route")
+                .setCount(10)
+                .setIntervalSec(1)
+        );
+        rules.add(new GatewayFlowRule("aliyun_route")
+                .setCount(2)
+                .setIntervalSec(2)
+                .setBurst(2)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_CLIENT_IP)
+                )
+        );
+        rules.add(new GatewayFlowRule("httpbin_route")
+                .setCount(10)
+                .setIntervalSec(1)
+                .setControlBehavior(RuleConstant.CONTROL_BEHAVIOR_RATE_LIMITER)
+                .setMaxQueueingTimeoutMs(600)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_HEADER)
+                        .setFieldName("X-Sentinel-Flag")
+                )
+        );
+        rules.add(new GatewayFlowRule("httpbin_route")
+                .setCount(1)
+                .setIntervalSec(1)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
+                        .setFieldName("pa")
+                )
+        );
+        rules.add(new GatewayFlowRule("httpbin_route")
+                .setCount(2)
+                .setIntervalSec(30)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
+                        .setFieldName("type")
+                        .setPattern("warn")
+                        .setMatchStrategy(SentinelGatewayConstants.PARAM_MATCH_STRATEGY_CONTAINS)
+                )
+        );
+
+        rules.add(new GatewayFlowRule("some_customized_api")
+                .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
+                .setCount(5)
+                .setIntervalSec(1)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
+                        .setFieldName("pn")
+                )
+        );
+        GatewayRuleManager.loadRules(rules);
+    }
+}
