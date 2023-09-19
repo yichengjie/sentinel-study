fa6fa061274bbb031a6c74b45de8d9daaea3cc03
yichengjie <626659321@qq.com>
2023-09-19 11:45:45 +0800

    async task test

---
+++ hello-async-task/src/main/java/com/yicj/async/AsyncTaskApplication.java
@@ -0,0 +1,12 @@
+package com.yicj.async;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncTaskApplication.class, args) ;
+    }
+}
