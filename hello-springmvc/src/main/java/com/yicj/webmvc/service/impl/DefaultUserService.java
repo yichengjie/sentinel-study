cc0c0047391e18f9261e5e5159730abe46b40ad3
易成杰 <626659321@qq.com>
2023-09-22 09:28:16 +0800

    Qualifier usage

--- hello-springmvc/src/main/java/com/yicj/webmvc/service/impl/DefaultUserService.java
+++ hello-springmvc/src/main/java/com/yicj/webmvc/service/impl/DefaultUserService.java
@@ -2,21 +2,37 @@ package com.yicj.webmvc.service.impl;
 
 import com.yicj.webmvc.anno.HelloAnnotation;
 import com.yicj.webmvc.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-
 /**
  * @author: yicj
  * @date: 2023/9/17 10:14
  */
+@Slf4j
 @Service
 @Qualifier
 //@HelloAnnotation
-public class DefaultUserService implements UserService {
+public class DefaultUserService implements UserService, EnvironmentAware, InitializingBean {
+
+    private Environment environment ;
 
     @Override
     public String hello() {
 
         return "default hello world";
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment ;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("initializing bean");
     }
 }
